019731572439698381110304ce56a3a87ae2d91f
yeroc790 <yeroc790@users.noreply.github.com>
2017-12-05 12:06:49 -0600

    Create login.php

---
+++ login/login.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Remote Login</title>
+	</head>
+	<body>
+		<form method="POST" action="login-exec.php">
+			<label for="username">Username</label>
+			<input type="text" name="username">
+			<label for="password">Password</label>
+			<input type="password" name="password">
+			<input type="submit" name="login" value="Login">
+		</form>
+	</body>
+</html>
